949ce9b18a952fc8375746aa33ff728cf98d569b
Frank Chang <frankchang0125@gmail.com>
2025-01-05 10:15:02 +0800

    NexT: Update global configs
    Update configs:
      * Disable home page banner string and cover image.
      * Remove home page banner string.
      * Change the navigation type to autoCollapse.
      * Disable right side bar.
      * Disable related article recommendations.
      * Disable article copyright.
    
    Signed-off-by: Frank Chang <frankchang0125@gmail.com>

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,13 +1,8 @@
 const CONFIG = {
-  NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
-  NEXT_HOME_BANNER_STRINGS: [
-    'Hi，我是一个程序员',
-    'Hi，我是一个打工人',
-    'Hi，我是一个干饭人',
-    '欢迎来到我的博客🎉'
-  ], // 首页大图标语文字
+  NEXT_HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
+  NEXT_HOME_BANNER_STRINGS: [], // 首页大图标语文字
 
-  NEXT_NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
+  NEXT_NAV_TYPE: 'autoCollapse', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
   NEXT_POST_LIST_COVER: false, // 文章列表显示封面图
   NEXT_POST_LIST_PREVIEW: true, // 显示文章预览
@@ -18,7 +13,7 @@ const CONFIG = {
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
-  NEXT_RIGHT_BAR: true, // 是否显示右侧栏
+  NEXT_RIGHT_BAR: false, // 是否显示右侧栏
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
@@ -36,8 +31,8 @@ const CONFIG = {
   NEXT_WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
-  NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
-  NEXT_ARTICLE_COPYRIGHT: true, // 文章版权声明
+  NEXT_ARTICLE_RELATE_POSTS: false, // 相关文章推荐
+  NEXT_ARTICLE_COPYRIGHT: false, // 文章版权声明
   NEXT_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   NEXT_ARTICLE_INFO: true // 显示文章信息
 }
