0503a0a1d1d6ee0dbf2c572d91037266229e560f
Frank Chang <frankchang0125@gmail.com>
2025-01-12 11:56:29 +0800

    Update blog font
    Signed-off-by: Frank Chang <frankchang0125@gmail.com>

--- conf/font.config.js
+++ conf/font.config.js
@@ -22,6 +22,7 @@ module.exports = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"PingFang TC"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
